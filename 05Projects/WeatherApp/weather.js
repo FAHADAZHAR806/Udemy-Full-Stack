55ed3ef0c515cf99050a40c8ad4abf12f0ba744f
FADI <fahadkiyani28@gmail.com>
2026-01-30 18:10:08 +0500

    weather

--- 05Projects/WeatherApp/weather.js
+++ 05Projects/WeatherApp/weather.js
@@ -6,8 +6,38 @@ document.addEventListener("DOMContentLoaded", () => {
   const temperature = document.getElementById("temperature");
   const description = document.getElementById("description");
   const errorMessage = document.getElementById("error-message");
-});
-getWeatherBtn.addEventListener("click", () => {
-  const city = cityInput.value.trim();
-  if (!city) return;
+  const API_KEY = "4d062f7d83525ed272ead8243481fddb";
+
+  getWeatherBtn.addEventListener("click", async () => {
+    const city = cityInput.value.trim();
+    if (!city) return;
+    try {
+      const weatherData = await fetchWeatherData(city);
+      displayWeatherData(weatherData);
+    } catch (error) {
+      showError();
+    }
+  });
+  async function fetchWeatherData(city) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+    const response = await fetch(url);
+    console.log(typeof response);
+    console.log("RESPONSE", response);
+    if (!response.ok) {
+      throw new Error("City not found");
+    }
+    const data = await response.json();
+    return data;
+  }
+  function displayWeatherData(data) {
+    const { name, main, weather } = data;
+    cityNameDisplay.textContent = name;
+    temperature.textContent = `Temperature : ${main.temp}`;
+    description.textContent = `Weather : ${weather[0].description}`;
+    weatherInfo.classList.remove("hidden");
+  }
+  function showError() {
+    weatherInfo.classList.add("hidden");
+    errorMessage.classList.remove("hidden");
+  }
 });
