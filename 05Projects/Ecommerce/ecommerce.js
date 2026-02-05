f504917612ab0d62c1ebedeec80ec2505b37b311
FADI <fahadkiyani28@gmail.com>
2026-02-05 18:19:12 +0500

    eccomerce-proj

--- 05Projects/Ecommerce/ecommerce.js
+++ 05Projects/Ecommerce/ecommerce.js
@@ -3,17 +3,17 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       id: 1,
       name: "Product1",
-      price: "$40",
+      price: 40,
     },
     {
       id: 2,
       name: "Product2",
-      price: "$90",
+      price: 90,
     },
     {
       id: 3,
       name: "Product3",
-      price: "$150",
+      price: 150,
     },
   ];
   const cart = [];
@@ -26,11 +26,44 @@ document.addEventListener("DOMContentLoaded", () => {
   products.forEach((product) => {
     const productDiv = document.createElement("div");
     productDiv.classList.add("product");
-    productDiv.innerHTML = `
-    <span>${product.name} - ${product.price}</span>
-    <button data-id="${product.id}"> Add to Cart</button>`;
+    productDiv.innerHTML = ` 
+    <span>${product.name} - $${product.price} </span>
+    <button data-id="${product.id}"> Add to Cart </button>  `;
     productList.appendChild(productDiv);
   });
 
-  // checkoutBtn.addEventListener("click", () => {});
+  productList.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
+      const productID = parseInt(e.target.getAttribute("data-id"));
+      const product = products.find((p) => p.id === productID);
+      addToCart(product);
+    }
+  });
+  function addToCart(product) {
+    cart.push(product);
+    renderCart();
+  }
+  function renderCart() {
+    cartItems.innerText = "";
+    let totalprice = 0;
+    if (cart.length) {
+      emptyCart.classList.add("hidden");
+      cartTotal.classList.remove("hidden");
+      cart.forEach((item, index) => {
+        totalprice += item.price;
+        const cartItem = document.createElement("div");
+        cartItem.innerHTML = `${item.name} - ${item.price}`;
+        cartItems.appendChild(cartItem);
+        totalPrice.textContent = `$${totalprice}`;
+      });
+    } else {
+      emptyCart.classList.remove("hidden");
+      totalPrice.textContent = `$0.00`;
+    }
+  }
+  checkoutBtn.addEventListener("click", () => {
+    cart.length = 0;
+    alert("check out successfully");
+    renderCart();
+  });
 });
